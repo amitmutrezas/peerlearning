d0e296bd6f527934e7032885afa1f73e0ceb26bf
Amit <amitmutrezas@gmail.com>
2016-07-16 22:36:34 +0530

    First commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ActionController::Base
+
+  def create
+    user = User.from_omniauth(auth_hash)
+    session[:current_user_id] = auth_hash['uid']
+    # session[:current_user_hash] = auth_hash["info"]["user"]
+    session[:current_user_access_token] = auth_hash['credentials']['token']
+    redirect_to "http://localhost:3000/users/sign_in?guest_login=true&email=gueststu@avanti.in&password=avanti"
+    # redirect_to root_url
+  end
+
+  def destroy
+    session[:current_user_id] = nil
+    redirect_to root_url
+  end
+
+  protected
+
+    def auth_hash
+      request.env['omniauth.auth']
+    end
+end
